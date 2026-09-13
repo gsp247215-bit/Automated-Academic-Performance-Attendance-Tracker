bbac04032e4021250e9741b50ec5da8e8b62dce0
gsp247215-bit <gsp247215@gmail.com>
2026-09-14 01:12:39 +0530

    Add InvalidDataException for handling invalid data

---
+++ model/model/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package com.academic.tracker.exception;
+
+public class InvalidDataException extends Exception {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
